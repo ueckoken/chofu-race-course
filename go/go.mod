43bbf2466713b852a4372a567b213cc93572713b
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-09-12 02:35:01 +0900

    implement user storage

--- go/go.mod
+++ go/go.mod
@@ -8,4 +8,11 @@ require (
 	google.golang.org/protobuf v1.28.1
 )
 
-require golang.org/x/text v0.3.7 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- go/pkg/storage/user.go
+++ go/pkg/storage/user.go
@@ -1,36 +1,51 @@
 package storage
 
 import (
+	"bufio"
 	"errors"
+	"os"
 	"sync"
 
+	v1 "github.com/ueckoken/chofu-race-course/go/_proto/spec/v1"
 	"github.com/ueckoken/chofu-race-course/go/pkg/store"
 
-	v1 "github.com/ueckoken/chofu-race-course/go/_proto/spec/v1"
+	"google.golang.org/protobuf/proto"
 )
 
 type Writer struct {
 	filePath string
-	mtx      *sync.Mutex
+	mu       *sync.Mutex
 }
 
 var _ store.User = (*Writer)(nil)
 var notFound = errors.New("record not found")
 
 func NewWriter(path string) (*Writer, error) {
-	return &Writer{filePath: path, mtx: &sync.Mutex{}}, nil
+	return &Writer{filePath: path, mu: &sync.Mutex{}}, nil
 }
 
 func (w *Writer) Create(u *v1.User) error {
 	if _, err := w.GetById(u.GetId()); err != nil {
 		return err
 	}
-	return nil
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	f, err := os.OpenFile(w.filePath, os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	b, err := proto.Marshal(u)
+	if err != nil {
+		return nil
+	}
+	_, err = f.Write(append(b, '\n'))
+	return err
 }
 
 // GetById は与えたIDを持つUserを返す。存在しないときはErrorを返す。
 func (w *Writer) GetById(id string) (*v1.User, error) {
-	us, err := w.getUsers()
+	us, err := w.readFromFile()
 	if err != nil {
 		return nil, err
 	}
@@ -42,6 +57,22 @@ func (w *Writer) GetById(id string) (*v1.User, error) {
 	return nil, notFound
 }
 
-func (w *Writer) getUsers() ([]*v1.User, error) {
-	return nil, nil
+func (w *Writer) readFromFile() ([]*v1.User, error) {
+	us := make([]*v1.User, 1)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	f, err := os.Open(w.filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		u := &v1.User{}
+		if err := proto.Unmarshal(sc.Bytes(), u); err != nil {
+			return nil, err
+		}
+		us = append(us, u)
+	}
+	return us, nil
 }
