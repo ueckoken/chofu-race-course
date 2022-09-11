61714af0290f9ff2ad20722c69fd5d653fbc1ef4
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-09-12 01:24:27 +0900

    fix

--- go/main.go
+++ go/main.go
@@ -10,9 +10,13 @@ import (
 )
 
 func main() {
-	err := http.ListenAndServe(
+	route, err := internal.NewRoute()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = http.ListenAndServe(
 		"localhost:8080",
-		h2c.NewHandler(internal.NewRoute(), &http2.Server{}),
+		h2c.NewHandler(route, &http2.Server{}),
 	)
 	if err != nil {
 		log.Fatal(err)
