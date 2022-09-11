e373fffdd24006988650d3d8cc450e940b232338
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-09-12 05:37:45 +0900

    fix field name

--- go/pkg/serv/userDataService.go
+++ go/pkg/serv/userDataService.go
@@ -28,7 +28,7 @@ func (s *Server) UserData(
 	ctx context.Context,
 	req *connectGo.Request[v1.UserDataRequest],
 ) (*connectGo.Response[v1.UserDataResponse], error) {
-	user, err := s.store.GetById(req.Msg.UserId)
+	user, err := s.store.GetById(req.Msg.GetId())
 	if err != nil {
 		return nil, err
 	}
