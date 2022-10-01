7ea8cf719939321ea27e8dfe031859ef7a06d848
gotti <gotti@mail.gotti.dev>
2022-10-01 13:06:26 +0900

    implement authorization

--- go/internal/route.go
+++ go/internal/route.go
@@ -1,14 +1,10 @@
 package internal
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"path"
-	"strings"
 
-	"github.com/bufbuild/connect-go"
 	"github.com/ueckoken/chofu-race-course/go/pkg/authorizer"
 	"github.com/ueckoken/chofu-race-course/go/pkg/file"
 	"github.com/ueckoken/chofu-race-course/go/pkg/handler"
@@ -16,30 +12,9 @@ import (
 	"github.com/ueckoken/chofu-race-course/go/_proto/spec/v1/v1connect"
 )
 
-type endpoints struct {
-	authRequiedEndpoints []string
-}
-
-// isNeedAuthForProcedure returns wheater procedure(endpoint) needs user authentication
-// procedure must have starting slash
-// procedure example:
-// - /spec.v1.UserDataService/UserData
-func (e *endpoints) isNeedUserAuthForProcedure(procedure string) bool {
-	for _, a := range e.authRequiedEndpoints {
-		if a == strings.Replace(procedure, "/", "", -1) {
-			return true
-		}
-	}
-	return false
-}
-
 func NewRoute(dataDir string) (*http.ServeMux, error) {
 	mux := http.NewServeMux()
 	userWriter, err := file.NewUserFile(path.Join(dataDir, "user"))
-	if err != nil {
-		return nil, err
-	}
-	u, err := handler.NewUserServer(userWriter)
 	if err != nil {
 		return nil, err
 	}
@@ -47,32 +22,10 @@ func NewRoute(dataDir string) (*http.ServeMux, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize authorizer, err=%w", err)
 	}
-	ep := endpoints{authRequiedEndpoints: []string{"/spec.v1.UserDataService/UserData", "/spec.v1.VoteService/Vote"}}
-	mux.Handle(v1connect.NewUserDataServiceHandler(u, connect.WithInterceptors(
-		connect.UnaryInterceptorFunc(
-			func(next connect.UnaryFunc) connect.UnaryFunc {
-				return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
-					if !ep.isNeedUserAuthForProcedure(req.Spec().Procedure) {
-						res, err := next(ctx, req)
-						return res, err
-					}
-					token := req.Header().Get("Bearer")
-					u, ok, err := a.Verify(token)
-					if err != nil {
-						//TODO: use sufficient error code
-						log.Printf("failed to read token, err=%s", err)
-						return nil, fmt.Errorf("failed to read token, unauthorized")
-					}
-					if !ok {
-						log.Printf("failed to verify, err=%s", err)
-						return nil, fmt.Errorf("verification failed, unauthorized")
-					}
-					c := context.WithValue(ctx, "User", u)
-					res, err := next(c, req)
-					return res, err
-				})
-			},
-		),
-	)))
+	u, err := handler.NewUserServer(userWriter, a)
+	if err != nil {
+		return nil, err
+	}
+	mux.Handle(v1connect.NewUserDataServiceHandler(u))
 	return mux, nil
 }
--- go/pkg/handler/userDataService.go
+++ go/pkg/handler/userDataService.go
@@ -2,17 +2,21 @@ package handler
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/ueckoken/chofu-race-course/go/pkg/file"
 
 	connectGo "github.com/bufbuild/connect-go"
 	v1 "github.com/ueckoken/chofu-race-course/go/_proto/spec/v1"
 	"github.com/ueckoken/chofu-race-course/go/_proto/spec/v1/v1connect"
+	"github.com/ueckoken/chofu-race-course/go/pkg/authorizer"
 )
 
 type User struct {
 	v1connect.UnimplementedUserDataServiceHandler
 	store UserStore
+	auth  authorizer.JWTIssuer
 }
 
 type UserStore interface {
@@ -20,19 +24,25 @@ type UserStore interface {
 	Create(u *v1.User) error
 }
 
-func NewUserServer(store UserStore) (*User, error) {
+func NewUserServer(store UserStore, issuer authorizer.JWTIssuer) (*User, error) {
 	w, err := file.NewUserFile("hoge")
 	if err != nil {
 		return nil, err
 	}
-	return &User{store: w}, nil
+	return &User{store: w, auth: issuer}, nil
 }
 
-func (s *User) UserData(
-	ctx context.Context,
-	req *connectGo.Request[v1.UserDataRequest],
-) (*connectGo.Response[v1.UserDataResponse], error) {
-	user, err := s.store.GetById(req.Msg.GetId())
+func (s *User) UserData(ctx context.Context, req *connectGo.Request[v1.UserDataRequest]) (*connectGo.Response[v1.UserDataResponse], error) {
+	u, ok, err := s.auth.Verify(req.Msg.GetJwt().GetToken())
+	if err != nil {
+		log.Printf("auth verify returned error, err=%s\n", err)
+		return nil, connectGo.NewError(connectGo.CodeUnauthenticated, fmt.Errorf("failed to verify jwt key, maybe invalid"))
+	}
+	if !ok {
+		log.Printf("auth returned not ok, err=%s\n", err)
+		return nil, connectGo.NewError(connectGo.CodeUnauthenticated, fmt.Errorf("failed to authorize, maybe expired?"))
+	}
+	user, err := s.store.GetById(u)
 	if err != nil {
 		return nil, connectGo.NewError(connectGo.CodeNotFound, err)
 	}
