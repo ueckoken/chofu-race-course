6041ac948ca0ad229a03a7204766c52a6b6b2d20
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-10-08 00:23:52 +0900

    implement race database Update, and Delete.

--- go/pkg/envConfig/config.go
+++ go/pkg/envConfig/config.go
@@ -1,25 +1,20 @@
 package envConfig
 
 import (
-	"fmt"
-
 	"github.com/caarlos0/env/v6"
 )
 
 type EnvVar struct {
-	DataDir            string   `env:"DATADIR"`
+	DataDir            string   `env:"DATADIR,notEmpty"`
 	ListenAddr         string   `env:"LISTEN_ADDR" envDefault:"127.0.0.1:8080"`
-	AdminPassword      string   `env:"ADMIN_PASSWORD"`
-	CorsArrowedOrigins []string `env:"CORS_ARROWED_ORIGINS" envSeparator:":"`
+	AdminPassword      string   `env:"ADMIN_PASSWORD,notEmpty"`
+	CorsArrowedOrigins []string `env:"CORS_ARROWED_ORIGINS" envSeparator:"," envDefault:"localhost"`
 }
 
 func Parse() (*EnvVar, error) {
 	cfg := EnvVar{}
 	if err := env.Parse(&cfg); err != nil {
 		return nil, err
-	}
-	if cfg.AdminPassword == "" {
-		return nil, fmt.Errorf("admin password is blank, please specify it by ADMIN_PASSWORD")
 	}
 	return &cfg, nil
 }
--- go/pkg/file/race_test.go
+++ go/pkg/file/race_test.go
@@ -13,10 +13,12 @@ import (
 )
 
 func TestRaceFlow(t *testing.T) {
-	r, err := file.NewRaceFile(filepath.Join(t.TempDir(), "race-test"))
+	path := filepath.Join(t.TempDir(), "race-test")
+	r, err := file.NewRaceFile(path)
 	assert.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Implements(t, (*handler.RaceStore)(nil), r)
+	assert.FileExists(t, path)
 
 	res, err := r.GetById(0)
 	assert.Nil(t, res)
@@ -51,4 +53,46 @@ func TestRaceFlow(t *testing.T) {
 	assert.NotNil(t, rd)
 	assert.NotNil(t, rd.GetData())
 	assert.Equal(t, uint32(10), rd.GetData().GetId())
+}
+func TestUpdate(t *testing.T) {
+	r, err := file.NewRaceFile(filepath.Join(t.TempDir(), "update"))
+	require.NotNil(t, r)
+	require.NoError(t, err)
+	test1 := &v1.RaceDetail{
+		Data: &v1.Race{
+			Id:         1,
+			Name:       "テスト1",
+			Order:      1,
+			Start:      timestamppb.Now(),
+			IsFinished: false,
+		},
+	}
+	err = r.Create(test1)
+	require.NoError(t, err)
+
+	err = r.Update(&v1.RaceDetail{Data: &v1.Race{
+		Id:    2,
+		Name:  "テスト2",
+		Order: 1,
+	}})
+	assert.Error(t, err, "IDに紐づくレコードが存在しないのでエラー")
+
+	start := timestamppb.Now()
+	err = r.Update(&v1.RaceDetail{
+		Data: &v1.Race{
+			Id:    1,
+			Name:  "更新のテスト",
+			Order: 1,
+			Start: start,
+		},
+		VoteBegin: start,
+		VoteEnd:   start,
+	},
+	)
+	assert.NoError(t, err)
+	rd, _ := r.GetById(1)
+	assert.Equal(t, "更新のテスト", rd.GetData().GetName())
+
+	rds, _ := r.GetAll()
+	assert.Len(t, rds, 1, "レコード数は変化しない")
 }
