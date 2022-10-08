9980739c2b53a6b33ac205dce4b1f96d5ed4e67a
hutinoatari <63452799+hutinoatari@users.noreply.github.com>
2022-10-08 20:12:52 +0900

    SSGに書き直した

--- client/src/pages/horse.tsx
+++ client/src/pages/horse.tsx
@@ -1,23 +1,24 @@
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 import Link from "next/link";
+import { createPromiseClient } from "@bufbuild/connect-web";
+import { transport } from "../util/use-client";
 import { HorseDataService } from "../../_proto/spec/v1/userdata_connectweb";
-import { useClient } from "../util/use-client";
-import { AllHorseDataResponse } from "../../_proto/spec/v1/userdata_pb";
+import { AllHorseDataResponse, Horse } from "../../_proto/spec/v1/userdata_pb";
+import { GetStaticProps } from "next";
+import { JsonValue } from "@bufbuild/protobuf";
 
-const HorsePage: FC<{}> = () => {
-    const client = useClient(HorseDataService);
-    const [data, setData] = useState<AllHorseDataResponse | null>(null);
-    useEffect(() => {
-        client.allHorseData({}).then((res) => setData(res));
-    }, []);
+interface Props {
+    json: JsonValue;
+}
 
-    if (!data) return <p>読み込み中です。</p>;
+const HorsePage: FC<Props> = ({ json }) => {
+    const data = AllHorseDataResponse.fromJson(json);
     return (
         <>
             <h2>競争馬一覧</h2>
             <ul>
-                {data.horses.map((horseData) => (
-                    <li key={`race${horseData.id}`}>
+                {data!.horses.map((horseData) => (
+                    <li key={`horse${horseData.id}`}>
                         <Link href={`horse/${horseData.id}`}>
                             <a>{horseData.name}</a>
                         </Link>
@@ -26,6 +27,16 @@ const HorsePage: FC<{}> = () => {
             </ul>
         </>
     );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+    const client = createPromiseClient(HorseDataService, transport);
+    const res = await client.allHorseData({});
+    return {
+        props: {
+            json: res.toJson(),
+        },
+    };
 };
 
 export default HorsePage;
