a7148c566ce7ac8e266f59163bbed2325487f68f
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-10-12 09:46:26 +0900

    rewrite connect go response

--- go/pkg/handler/userDataService.go
+++ go/pkg/handler/userDataService.go
@@ -28,8 +28,8 @@ func NewUserServer(store UserStore, issuer authorizer.JWTIssuer, adminauth autho
 	return &User{store: store, auth: issuer, adminauth: adminauth}, nil
 }
 
-func (s *User) UserData(ctx context.Context, req *connectGo.Request[v1.UserDataRequest]) (*connectGo.Response[v1.UserDataResponse], error) {
-	u, ok, err := s.auth.Verify(req.Msg.GetJwt().GetToken())
+func (w *User) UserData(ctx context.Context, req *connectGo.Request[v1.UserDataRequest]) (*connectGo.Response[v1.UserDataResponse], error) {
+	u, ok, err := w.auth.Verify(req.Msg.GetJwt().GetToken())
 	if err != nil {
 		log.Printf("auth verify returned error, err=%s\n", err)
 		return nil, connectGo.NewError(connectGo.CodeUnauthenticated, fmt.Errorf("failed to verify jwt key, maybe invalid"))
@@ -38,33 +38,33 @@ func (s *User) UserData(ctx context.Context, req *connectGo.Request[v1.UserDataR
 		log.Printf("auth returned not ok, err=%s\n", err)
 		return nil, connectGo.NewError(connectGo.CodeUnauthenticated, fmt.Errorf("failed to authorize, maybe expired?"))
 	}
-	user, err := s.store.GetById(u)
+	user, err := w.store.GetById(u)
 	if err != nil {
 		return nil, connectGo.NewError(connectGo.CodeNotFound, err)
 	}
-	return &connectGo.Response[v1.UserDataResponse]{Msg: &v1.UserDataResponse{User: user}}, nil
+	return connectGo.NewResponse(&v1.UserDataResponse{User: user}), nil
 }
 
-func (u *User) CreateUser(ctx context.Context, req *connectGo.Request[v1.CreateUserRequest]) (*connectGo.Response[v1.CreateUserResponse], error) {
+func (w *User) CreateUser(ctx context.Context, req *connectGo.Request[v1.CreateUserRequest]) (*connectGo.Response[v1.CreateUserResponse], error) {
 	buf := make([]byte, 8)
 	if _, err := rand.Read(buf); err != nil {
 		log.Printf("failed to generate random userid, err=%s\n", err)
 		return nil, fmt.Errorf("username generation error")
 	}
 	uid := &v1.User{Id: fmt.Sprintf("%x", buf)}
-	err := u.store.Create(uid)
+	err := w.store.Create(uid)
 	if err != nil {
 		return nil, connectGo.NewError(connectGo.CodeInvalidArgument, err)
 	}
-	j, err := u.auth.Sign(uid.GetId())
+	j, err := w.auth.Sign(uid.GetId())
 	if err != nil {
 		return nil, connectGo.NewError(connectGo.CodeInvalidArgument, err)
 	}
-	return &connectGo.Response[v1.CreateUserResponse]{Msg: &v1.CreateUserResponse{User: uid, Jwt: &v1.JWT{Token: j}}}, nil
+	return connectGo.NewResponse(&v1.CreateUserResponse{User: uid, Jwt: &v1.JWT{Token: j}}), nil
 }
 
-func (c *User) LoginAsAdmin(ctx context.Context, req *connectGo.Request[v1.LoginAsAdminRequest]) (*connectGo.Response[v1.LoginAsAdminResponse], error) {
-	ok := c.adminauth.VerifyPassword(req.Msg.Password)
+func (w *User) LoginAsAdmin(ctx context.Context, req *connectGo.Request[v1.LoginAsAdminRequest]) (*connectGo.Response[v1.LoginAsAdminResponse], error) {
+	ok := w.adminauth.VerifyPassword(req.Msg.Password)
 	if !ok {
 		return nil, connectGo.NewError(connectGo.CodePermissionDenied, fmt.Errorf("failed to verify password"))
 	}
@@ -73,9 +73,9 @@ func (c *User) LoginAsAdmin(ctx context.Context, req *connectGo.Request[v1.Login
 		log.Printf("failed to generate random admin userid, err=%s\n", err)
 		return nil, fmt.Errorf("username generation error")
 	}
-	j, err := c.adminauth.Sign(fmt.Sprintf("%x", buf))
+	j, err := w.adminauth.Sign(fmt.Sprintf("%x", buf))
 	if err != nil {
 		return nil, connectGo.NewError(connectGo.CodeAborted, fmt.Errorf("failed to generate jwt"))
 	}
-	return &connectGo.Response[v1.LoginAsAdminResponse]{Msg: &v1.LoginAsAdminResponse{AdminJwt: &v1.JWT{Token: j}}}, nil
+	return connectGo.NewResponse(&v1.LoginAsAdminResponse{AdminJwt: &v1.JWT{Token: j}}), nil
 }
