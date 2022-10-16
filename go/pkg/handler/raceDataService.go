1d83d8637e8b5fbd7abf656d7db9d6ca0aba4f2d
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-10-17 01:02:17 +0900

    impl EditRace

--- go/pkg/handler/raceDataService.go
+++ go/pkg/handler/raceDataService.go
@@ -2,11 +2,13 @@ package handler
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	connect_go "github.com/bufbuild/connect-go"
 	v1 "github.com/ueckoken/chofu-race-course/go/_proto/spec/v1"
 	"github.com/ueckoken/chofu-race-course/go/_proto/spec/v1/v1connect"
+
 	"github.com/ueckoken/chofu-race-course/go/pkg/authorizer"
 )
 
@@ -20,6 +22,7 @@ type RaceStore interface {
 	GetAll() (*v1.RaceDetails, error)
 	GetByID(id uint32) (*v1.RaceDetail, error)
 	Create(*v1.RaceDetail) error
+	Update(*v1.RaceDetail) error
 }
 
 func NewRaceServer(store DataStore, adminauth authorizer.AdminAuthorizer) (*Race, error) {
@@ -47,6 +50,7 @@ func (r *Race) RaceData(_ context.Context, req *connect_go.Request[v1.RaceDataRe
 	}
 	return connect_go.NewResponse(&v1.RaceDataResponse{Race: rd}), nil
 }
+
 func (r *Race) RegisterRace(_ context.Context, req *connect_go.Request[v1.RegisterRaceRequest]) (*connect_go.Response[v1.RegisterRaceResponse], error) {
 	_, ok, err := r.adminAuth.Verify(req.Msg.GetAdminJwt().GetToken())
 	if err != nil {
@@ -76,6 +80,79 @@ func (r *Race) RegisterRace(_ context.Context, req *connect_go.Request[v1.Regist
 		return nil, connect_go.NewError(connect_go.CodeInternal, err)
 	}
 	return connect_go.NewResponse(&v1.RegisterRaceResponse{}), nil
+}
+
+func (r *Race) RegisterRaceResult(ctx context.Context, req *connect_go.Request[v1.RegisterRaceResultRequest]) (*connect_go.Response[v1.RegisterRaceResultResponse], error) {
+	return nil, connect_go.NewError(connect_go.CodeUnimplemented, errors.New("spec.v1.RaceDataService.RegisterRaceResult is not implemented"))
+}
+
+func (r *Race) EditRace(ctx context.Context, req *connect_go.Request[v1.EditRaceRequest]) (*connect_go.Response[v1.EditRaceResponse], error) {
+	_, ok, err := r.adminAuth.Verify(req.Msg.GetAdminJwt().GetToken())
+	if err != nil {
+		return nil, connect_go.NewError(connect_go.CodePermissionDenied, err)
+	}
+	if !ok {
+		return nil, connect_go.NewError(connect_go.CodePermissionDenied, fmt.Errorf("invalid jwt, maybe expired"))
+	}
+	if err := req.Msg.ValidateAll(); err != nil {
+		return nil, connect_go.NewError(connect_go.CodeInvalidArgument, err)
+	}
+	oldRec, err := r.store.Race.GetByID(req.Msg.GetId())
+	if err != nil {
+		return nil, connect_go.NewError(connect_go.CodeInternal, err)
+	}
+
+	editFields := findFieldsToEdit(req.Msg)
+	if editFields&EditRaceRequestName != 0 {
+		oldRec.Data.Name = req.Msg.GetName()
+	}
+	if editFields&EditRaceRequestOrder != 0 {
+		oldRec.Data.Order = req.Msg.GetOrder()
+	}
+	if editFields&EditRaceRequestStart != 0 {
+		oldRec.Data.Start = req.Msg.GetStart()
+	}
+	if editFields&EditRaceRequestDescription != 0 {
+		oldRec.Description = req.Msg.GetDescription()
+	}
+	if editFields&EditRaceRequestMembers != 0 {
+		oldRec.Members = req.Msg.GetMembers()
+	}
+
+	if err := r.store.Race.Update(oldRec); err != nil {
+		return nil, connect_go.NewError(connect_go.CodeInternal, err)
+	}
+	return connect_go.NewResponse(&v1.EditRaceResponse{}), nil
+}
+
+type EditRaceRequestField int
+
+const (
+	EditRaceRequestName        EditRaceRequestField = 1 << iota
+	EditRaceRequestOrder       EditRaceRequestField = 1 << iota
+	EditRaceRequestStart       EditRaceRequestField = 1 << iota
+	EditRaceRequestDescription EditRaceRequestField = 1 << iota
+	EditRaceRequestMembers     EditRaceRequestField = 1 << iota
+)
+
+func findFieldsToEdit(req *v1.EditRaceRequest) EditRaceRequestField {
+	var res EditRaceRequestField
+	if req.GetName() != "" {
+		res |= EditRaceRequestName
+	}
+	if req.GetOrder() != 0 {
+		res |= EditRaceRequestOrder
+	}
+	if req.GetStart() != nil {
+		res |= EditRaceRequestStart
+	}
+	if req.GetDescription() != "" {
+		res |= EditRaceRequestDescription
+	}
+	if len(req.Members) != 0 {
+		res |= EditRaceRequestMembers
+	}
+	return res
 }
 
 func raceDetails2Races(rds []*v1.RaceDetail) []*v1.Race {
