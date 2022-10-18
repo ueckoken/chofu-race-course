f1802ad2dee3a87cb9ebaebe4da93f85b4af706f
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-10-18 16:52:01 +0900

    implement add race result

--- go/pkg/handler/raceDataService.go
+++ go/pkg/handler/raceDataService.go
@@ -83,7 +83,38 @@ func (r *Race) RegisterRace(_ context.Context, req *connect_go.Request[v1.Regist
 }
 
 func (r *Race) RegisterRaceResult(ctx context.Context, req *connect_go.Request[v1.RegisterRaceResultRequest]) (*connect_go.Response[v1.RegisterRaceResultResponse], error) {
-	return nil, connect_go.NewError(connect_go.CodeUnimplemented, errors.New("spec.v1.RaceDataService.RegisterRaceResult is not implemented"))
+	_, ok, err := r.adminAuth.Verify(req.Msg.GetAdminJwt().GetToken())
+	if err != nil {
+		return nil, connect_go.NewError(connect_go.CodePermissionDenied, err)
+	}
+	if !ok {
+		return nil, connect_go.NewError(connect_go.CodePermissionDenied, fmt.Errorf("invalid jwt, maybe expired"))
+	}
+	if err := req.Msg.ValidateAll(); err != nil {
+		return nil, connect_go.NewError(connect_go.CodeInvalidArgument, err)
+	}
+	if len(req.Msg.GetMembers()) == 0 {
+		return nil, connect_go.NewError(connect_go.CodeInvalidArgument, fmt.Errorf("members array length 0 is nil"))
+	}
+	raceRec, err := r.store.Race.GetByID(req.Msg.GetId())
+	if err != nil {
+		return nil, connect_go.NewError(connect_go.CodeInternal, err)
+	}
+	raceRec.Members = req.Msg.GetMembers()
+	if err := r.store.Race.Update(raceRec); err != nil {
+		return nil, connect_go.NewError(connect_go.CodeInternal, err)
+	}
+
+	for i, horse := range req.Msg.GetMembers() {
+		hd, err := r.getFixHorseDetail(horse, raceRec)
+		if err != nil {
+			return nil, connect_go.NewError(connect_go.CodeInternal, fmt.Errorf("updateHorseDetail failed in `%d`, %w", i, err))
+		}
+		if err := r.store.Horse.Update(hd); err != nil {
+			return nil, connect_go.NewError(connect_go.CodeInternal, fmt.Errorf("horse update failed, %w", err))
+		}
+	}
+	return connect_go.NewResponse(&v1.RegisterRaceResultResponse{}), nil
 }
 
 func (r *Race) EditRace(ctx context.Context, req *connect_go.Request[v1.EditRaceRequest]) (*connect_go.Response[v1.EditRaceResponse], error) {
@@ -155,16 +186,48 @@ func findFieldsToEdit(req *v1.EditRaceRequest) EditRaceRequestField {
 	return res
 }
 
+func raceDetail2Race(rd *v1.RaceDetail) *v1.Race {
+	return &v1.Race{
+		Id:         rd.GetData().GetId(),
+		Name:       rd.GetData().GetName(),
+		Order:      rd.GetData().GetOrder(),
+		Start:      rd.GetData().GetStart(),
+		IsFinished: rd.GetData().GetIsFinished(),
+	}
+}
 func raceDetails2Races(rds []*v1.RaceDetail) []*v1.Race {
-	rs := []*v1.Race{}
-	for _, rd := range rds {
-		rs = append(rs, &v1.Race{
-			Id:         rd.GetData().GetId(),
-			Name:       rd.GetData().GetName(),
-			Order:      rd.GetData().GetOrder(),
-			Start:      rd.GetData().GetStart(),
-			IsFinished: rd.GetData().GetIsFinished(),
-		})
+	rs := make([]*v1.Race, len(rds))
+	for i, rd := range rds {
+		rs[i] = raceDetail2Race(rd)
 	}
 	return rs
+}
+
+func (r *Race) getFixHorseDetail(horse *v1.RaceDetail_Member, rd *v1.RaceDetail) (*v1.HorseDetail, error) {
+	hd, err := r.store.Horse.GetByID(horse.GetHorse().GetId())
+	if err != nil {
+		return nil, connect_go.NewError(connect_go.CodeInternal, err)
+	}
+	switch o := horse.GetOrder().GetOrderOneof().(type) {
+	case *v1.RaceOrder_Order:
+		if o.Order == 1 {
+			hd.Wins++
+		}
+		if 1 <= o.Order && o.Order <= 4 {
+			hd.Matches++
+		}
+	case *v1.RaceOrder_Note:
+		if o.Note == v1.RaceOrder_NOTE_TYPE_GIVEUP {
+			hd.Matches++
+		}
+	default:
+		return nil, connect_go.NewError(connect_go.CodeAlreadyExists, errors.New("hoge"))
+	}
+	hd.Histories = append(hd.Histories, &v1.HorseDetail_History{
+		Race: raceDetail2Race(rd),
+		// このOrderはレースが第nレースかを表現
+		Order:  rd.GetData().GetOrder(),
+		Result: horse.GetOrder(),
+	})
+	return hd, nil
 }
